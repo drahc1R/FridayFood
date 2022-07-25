bdac768cd407722b1fd004dafa2a8df2ad95a447
Richard Bann <richard@bann.com>
2022-07-25 15:44:02 -0300

    Revert "Revert "update""
    This reverts commit d019d93c4ca807b7625cb73361be40be50894766.

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      name: 'Glazed Donut',
+      slug: 'glazed-donut',
+      category: 'Donuts',
+      image: '/images/glazeddonut.jpg', //856 x 564
+      price: 1.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'A delicious glazed donut',
+    },
+    {
+      name: 'Persimmon Cake',
+      slug: 'persimmon-cake',
+      category: 'Cakes',
+      image: '/images/persimmoncake.jpg',
+      price: 1.99,
+      countInStock: 5,
+      rating: 4.7,
+      numReviews: 14,
+      description: 'A delicious chocolate cookie',
+    },
+    {
+      name: 'Vietnamese Iced Coffee Cake',
+      slug: 'vietnamese-iced-coffee-cake',
+      category: 'Cakes',
+      image: '/images/coffeecake.jpg',
+      price: 14.99,
+      countInStock: 10,
+      rating: 4.8,
+      numReviews: 10,
+      description: 'A delicious Vietnamese Iced Coffee Cake',
+    },
+  ],
+};
+export default data;
