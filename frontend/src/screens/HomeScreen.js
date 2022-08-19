cf4b3d08c851dcae32a0937fa49dab85a2f2b015
Richard Bann <richard@bann.com>
2022-08-19 19:04:55 -0300

    deploy

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import { useReducer, useEffect } from 'react';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Products';
@@ -22,7 +21,7 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, products, error }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, products, error }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
